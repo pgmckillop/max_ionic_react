d29dc28a5670834868bdeb7958fcef56bc41f9c2
Paul McKillop <pgmckillop@gmail.com>
2020-10-07 06:47:43 +0100

    Theming

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -5,7 +5,7 @@ const BmiResult: React.FC<{ result: number }> = (props) => {
   return (
     <IonRow>
       <IonCol>
-        <IonCard>
+        <IonCard color="secondary">
           <IonCardContent className="ion-text-center">
             <h2>Your Body Mass Index (BMI)</h2>
             <h3>{props.result.toFixed(2)}</h3>
